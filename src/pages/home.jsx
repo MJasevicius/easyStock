d80e61ce996d486a9a0d71d255b87d2932df49c5
Sliver <matas.jasevicius@gmail.com>
2024-11-14 19:24:36 +0200

    Added bar chart
    Added bar chart to display finantial info

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import Pie from "../components/pie";
 import Important from "../components/important"
+import Bar from '../components/bar';
 
 const Home = () => {
     return (
-        <div className='container'>
+        <>
+        <div className="container">
             Pagrindinis
+        </div>
+        <div className='container'>
+            
 
             <div className="home-column">
                 <Pie />
                 <Important />
             </div>
+            <div className="home-column">
+                <Bar />
+            </div>
         </div>
+        </>
+        
     );
 }
 
