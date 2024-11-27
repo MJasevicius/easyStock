00012a5da8bed055a90f49c005784d7bb1654026
Sliver <matas.jasevicius@gmail.com>
2024-11-27 20:53:50 +0200

    pie chart integrated with api

--- src/components/pie.jsx
+++ src/components/pie.jsx
@@ -1,21 +1,29 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import { PieChart } from "@mui/x-charts/PieChart";
+import axios from "axios";
 
-const ACTIVE_PRODUCTS = 4521;
+const BASE_URL = "http://localhost:3000";
 
-const PIE_CHART_DATA = [
-  { id: 0, value: 10, label: 'Prekių netrūksta' },
-  { id: 1, value: 15, label: 'Prekių gali trūkti' },
-  { id: 2, value: 20, label: 'Prekių trūksta' },
-  { id: 3, value: 3, label: 'Prekių nebėra' }
-];
+
 
 const Pie = () => {
+  const [activeProducts, setActiveProducts] = useState(0)
+  const [notMissing, setNotMissing] = useState(0)
+  const [nearlyMissing, setNearlyMissing] = useState(0)
+  const [criticalLevel, setCriticalLevel] = useState(0)
+  const [missing, setMissing] = useState(0)
   const [innerRadius, setInnerRadius] = useState(55);
   const [isTitleVisible, setIsTitleVisible] = useState(true);
   const chartRef = useRef(null);
   const timeoutRef = useRef(null);
   const isMouseOverChart = useRef(false);
+
+  let PIE_CHART_DATA = [
+    { id: 0, value: notMissing, label: 'Prekių netrūksta' },
+    // { id: 1, value: nearlyMissing, label: 'Prekių gali trūkti' },
+    { id: 2, value: criticalLevel, label: 'Prekių trūksta' },
+    { id: 3, value: missing, label: 'Prekių nebėra' }
+  ];
 
   const handleMouseEnter = useCallback(() => {
     setInnerRadius(0);
@@ -49,6 +57,40 @@ const Pie = () => {
     }
   }, [chartRef, handleMouseEnter, handleMouseLeave]);
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/products`);
+        const products = response.data;
+        setActiveProducts(products.length);
+  
+        const notMissing = products.filter(
+          (product) => product.count > product.alert_level
+        ).length;
+  
+        const nearlyMissing = products.filter(
+          (product) => product.count > product.alert_level && product.count < product.alert_level * 1.25
+        ).length;
+  
+        const criticalLevel = products.filter(
+          (product) => product.count > 0 && product.count <= product.alert_level
+        ).length;
+  
+        const missing = products.filter(
+          (product) => product.count === 0
+        ).length;
+  
+        setNotMissing(notMissing);
+        setNearlyMissing(nearlyMissing);
+        setCriticalLevel(criticalLevel);
+        setMissing(missing);
+      } catch (error) {
+        console.error("Error fetching products:", error.message);
+      }
+    };
+  
+    fetchProducts();
+  }, []);
   
 
   return (
@@ -75,7 +117,7 @@ const Pie = () => {
       {isTitleVisible && (
         <div className="piechart-title">
           <div className="products-number">
-            {ACTIVE_PRODUCTS}
+            {activeProducts}
           </div>
           Produktai
         </div>
